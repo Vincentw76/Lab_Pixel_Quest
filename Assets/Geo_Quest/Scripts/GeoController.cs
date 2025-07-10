89f23acf4279dd0029022927820a45ac9639ff4c
Vincentw76 <Vincentwang427@gmail.com>
2025-07-10 12:17:58 -0400

    Class progression

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections.LowLevel.Unsafe;
@@ -7,15 +8,42 @@ using UnityEngine;
 
 public class GeoController : MonoBehaviour
 {
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("Hit");
+    }
+    public int speed;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World")
+
     }
+
+    
 
     // Update is called once per frame
     void Update()
     {
+        /*
+        float xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2 (xInput * speed, rb.velocity.y);
+        /*
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                rb.velocity = new Vector2(1, rb.velocity.y);
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                transform.position += new Vector3(0, -1, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                rb.velocity = new Vector2(-1, rb.velocity.y);
+            }
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                transform.position += new Vector3(0, 1, 0);
+            }
         
     }
 }
