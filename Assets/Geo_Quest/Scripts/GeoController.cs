095a25c79571a87e0f0a94852bbad0b5dc40c3b0
Vincentw76 <Vincentwang427@gmail.com>
2025-07-14 12:16:19 -0400

    More progres

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -1,49 +1,57 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Collections.LowLevel.Unsafe;
-using Unity.VisualScripting.FullSerializer;
-using UnityEditor;
+
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GeoController : MonoBehaviour
 {
+    Rigidbody2D rb;
+
+
+
     private void OnTriggerStay2D(Collider2D collision)
     {
-        Debug.Log("Hit");
+        switch (collision.tag)
+        {
+            case "Death":
+                {
+                    string thislevel = SceneManager.GetActiveScene().name;
+                    SceneManager.LoadScene(thislevel);
+                    break;
+                }
+        }
     }
     public int speed;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
 
     }
 
-    
+
 
     // Update is called once per frame
     void Update()
     {
-        /*
         float xInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2 (xInput * speed, rb.velocity.y);
-        /*
-            if (Input.GetKeyDown(KeyCode.D))
-            {
-                rb.velocity = new Vector2(1, rb.velocity.y);
-            }
-            if (Input.GetKeyDown(KeyCode.S))
-            {
-                transform.position += new Vector3(0, -1, 0);
-            }
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                rb.velocity = new Vector2(-1, rb.velocity.y);
-            }
-            if (Input.GetKeyDown(KeyCode.W))
-            {
-                transform.position += new Vector3(0, 1, 0);
-            }
-        
+        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.velocity = new Vector2(1, rb.velocity.y);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-1, rb.velocity.y);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
+
     }
-}
+}   
