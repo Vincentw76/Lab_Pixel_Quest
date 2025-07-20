ee9098232733635007891a501abf3672e7a19ece
Vincentw76 <Vincentwang427@gmail.com>
2025-07-19 22:11:09 -0400

    HW2

--- Assets/Hands-On Homework #2/Scripts/HW2PlayerShoot.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerShoot.cs
@@ -2,5 +2,45 @@ using UnityEngine;
 
 public class HW2PlayerShoot : MonoBehaviour
 {
+    public GameObject preFab;
+    public Transform bulletTrash;
+    public Transform bulletSpawn;
 
+    private const float Timer = 0.5f;
+    private float _currntTime = 0.5f;
+    private bool _canShoot = true;
+    private void Update()
+    {
+        TimerMethod();
+        Shoot();
+
+    }
+
+
+
+    private void TimerMethod()
+    {
+        if (!_canShoot)
+        {
+            _currntTime -= Time.deltaTime;
+
+            if (_currntTime < 0)
+            {
+                _canShoot = true;
+                _currntTime = Timer;
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && _canShoot)
+        {
+            GameObject bullet = Instantiate(preFab, bulletSpawn.position, Quaternion.identity);
+
+            bullet.transform.SetParent(bulletTrash);
+
+            _canShoot = false;
+        }
+    }
 }
